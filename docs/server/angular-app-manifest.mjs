4134656642af9f11f82266957f63b246c146207d
Omar-M-AlEbrahim <omaralebrahim719@gmail.com>
2025-03-20 21:45:00 +0300

    Build Angular project for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/UserDashbord',
+  baseHref: '/userDashbord/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 23579, hash: '6394f7999de0a3d4190d0470ace0d6e1ca51f1dbc5832b4f7145de248ebcf6fa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17153, hash: '05825c88067c4f2849459ece25b2bd3e54caa355660940aab0086756fa1f7a11', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23580, hash: 'e5e310988ff4cb29e171a479a28171afba4493e95a26834fe8ab555eb237d429', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17154, hash: '2ae90518f85846558b7f4e37c5edaa89caff9a603a20a5a5b1840512c9028333', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-WNKDDIZJ.css': {size: 6979, hash: 'AsSxQJi0uyE', text: () => import('./assets-chunks/styles-WNKDDIZJ_css.mjs').then(m => m.default)}
   },
 };
